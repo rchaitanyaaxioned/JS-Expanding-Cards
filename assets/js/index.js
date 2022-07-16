4f8fb9cf12dacf3e7ae3ba97c66a36e653559483
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-16 18:34:30 +0530

    FIX: JS logic fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,4 @@
-// var ul = document.getElementsByClassName("slider-img-list");
-// console.log(ul);
-// var li = document.getElementsByClassName("slider-img");
-// console.log("check before forloop");
-// for (var i = 0; i < li.length; i++) {
-//   console.log(i);
-//   li[i].addEventListener("click", function () {
-//     var current = document.getElementsByClassName("active");
-//     current[0].classList.remove("active");
-//     this.className += " active";
-//   });
-// }
-
-
 const slider = document.querySelectorAll(".slider-img");
-console.log(slider);
 
 slider.forEach(imgSelector);
 
