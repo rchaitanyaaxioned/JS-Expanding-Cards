da6cbea7c47b1e3d38b9ab652c95d2b7cebac605
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-16 17:29:16 +0530

    FIX: new js logic implimented

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,34 @@
-var ul = document.getElementsByClassName("slider-img-list");
-console.log(ul);
-var li = document.getElementsByClassName("slider-img");
-console.log("check before forloop");
-for (var i = 0; i < li.length; i++) {
-  console.log(i);
-  li[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].classList.remove("active");
-    this.className += " active";
-  });
+// var ul = document.getElementsByClassName("slider-img-list");
+// console.log(ul);
+// var li = document.getElementsByClassName("slider-img");
+// console.log("check before forloop");
+// for (var i = 0; i < li.length; i++) {
+//   console.log(i);
+//   li[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].classList.remove("active");
+//     this.className += " active";
+//   });
+// }
+
+
+const slider = document.querySelectorAll(".slider-img");
+console.log(slider);
+
+slider.forEach(imgSelector);
+
+function imgSelector(item) {
+  item.addEventListener("click", openImg);
+  function openImg() {
+    removeActive();
+    item.classList.add("active");
+  }
+}
+
+function removeActive() {
+  slider.forEach(removeClass);
+
+  function removeClass(item) {
+    item.classList.remove("active");
+  }
 }
